538c905b0033952b9884b583159c0c85ca4841e0
badusha061 <badubadusha061@gmail.com>
2024-04-27 22:26:29 +0530

    register page

--- server/chat/consumer.py
+++ server/chat/consumer.py
@@ -3,15 +3,52 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from chat.models import Conversation , Message
+from asgiref.sync import async_to_sync
+
 
 class Chat(AsyncWebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         print('connected')
         return super().connect()
     
-    def disconnect(self, code):
+    async def disconnect(self, code):
         print('disconnected')
         return super().disconnect(code)
+    
+
+    async def receive(self, text_data=None):
+        if text_data == None:
+            return False
+        
+        text_data_json = json.loads('text_data')
+        text = text_data_json['json']
+        sender = text_data_json['sender']
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': text,
+                'sender': sender
+            }
+        )
+
+
+        return await super().receive(text_data)
+    
+
+    async def chat_message(self,event):
+        text = event["message"]
+        sender = event["sender"]
+
+        self.send(text_data=json.dump({
+            'text':text,
+            'sender':sender
+        }))
+
+    
+    
+
+    
     
 
 
